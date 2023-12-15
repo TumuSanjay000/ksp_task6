f9f00c2ded3cae02ad1342608aba6d8aeb011df5
sanjay tumu <144722861+TumuSanjay000@users.noreply.github.com>
2023-12-15 20:54:09 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return (a+b)
+
+print("wellcome to task6 of carearx")
+
+n=10
+print(n)
+n1=int(input())
+if n1<=10:
+    print("n is less than 10")
+else:
+    print("n is not less tha 10")
+for i in range(n-5):
+    print("for loop iteration")
+#functions
+print(add(n,n1))
